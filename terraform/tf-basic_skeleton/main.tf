5885380f71c988f9bc6c8345c823ca86dc6db47d
Anupam <anupaminit@gmail.com>
2026-07-05 16:43:59 +0100

    feat: kzb85 bump tf version to 1.15.7

--- terraform/tf-basic_skeleton/main.tf
+++ terraform/tf-basic_skeleton/main.tf
@@ -1,8 +1,8 @@
-// Resource block to deploy VPC Network
-resource "google_compute_network" "tst_vpc" {
-  project                         = var.project_id
-  name                            = "tst-vpc"
-  routing_mode                    = "GLOBAL"
-  auto_create_subnetworks         = false
-  delete_default_routes_on_create = false
+// Resource block to enable google apis
+resource "google_project_service" "enable_api" {
+  count = length(var.enable_apis)
+
+  project            = var.project_id
+  service            = var.enable_apis[count.index]
+  disable_on_destroy = false
 }
